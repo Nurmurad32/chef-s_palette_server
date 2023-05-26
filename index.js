a961558d445b34a2838d9bf718f49d0430e49bb4
Nurmurad32 <nur.murad32@gmail.com>
2023-05-27 02:11:55 +0600

    server side almose done

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+
+const chefs = require('./data/chefs.json')
+const recipes = require('./data/recipes.json')
+
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Send category to the SERVER
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
+})
+
+// Send News to the SERVER
+app.get('/recipes', (req, res) => {
+  res.send(recipes)
+})
+
+// Send News to the SERVER by id
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedNews = chefs.find(n => parseInt(n.id) === id);
+  console.log(selectedNews)
+  res.send(selectedNews)
+})
+
+// Send News to the SERVER by id
+app.get('/recipes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id == 0) {
+    res.send(recipes)
+  }
+  else {
+    const selectedCategory = recipes.filter(n => parseInt(n.chef_id) === id);
+    res.send(selectedCategory)
+  }
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
